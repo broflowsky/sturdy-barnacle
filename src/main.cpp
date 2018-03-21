4c2b9df93984bfc530b797b71c7ce82c9f712430
neboskrebnut <ivanov.arseniy@gmail.com>
2018-03-21 14:21:18 -0400

    2

--- src/main.cpp
+++ src/main.cpp
@@ -15,37 +15,23 @@ int main(){
 	
 	GraphDirected *gd = new GraphDirected();
 
-	Vertex *v[5];
+	//Vertex *v[5];
 	for (size_t i = 0; i < 5; i++)
 	{
-		v[i] = new Vertex(1);
+		gd->add(*new Vertex(i));
 	}
-	
-	for (size_t i = 0; i < 5; i++)
-	{
-		gd->add(*v[i]);
-	}
-	
-	//gd->display();
-	Edge *e[6];
-	/*Invard
-	e[0] = new Edge(2, v[1], v[0]);
-	e[1] = new Edge(2, v[2], v[0]);
-	e[2] = new Edge(2, v[3], v[0]);
-	e[3] = new Edge(2, v[4], v[3]);
-	e[4] = new Edge(2, v[5], v[3]);
-	e[5] = new Edge(2, v[6], v[2]);*/
-	/*Outward*/
-	e[0] = new Edge(2, v[0], v[1]);
-	e[1] = new Edge(2, v[0], v[2]);
-	e[2] = new Edge(2, v[0], v[3]);
-	e[3] = new Edge(2, v[3], v[4]);
-	e[4] = new Edge(2, v[3], v[5]);
-	e[5] = new Edge(2, v[2], v[6]);
-	gd->add(*e, 6);
+
+	gd->add(gd->link(*gd->searchVertex(0), *gd->searchVertex(1)));
+	gd->add(gd->link(*gd->searchVertex(0), *gd->searchVertex(2)));
+	gd->add(gd->link(*gd->searchVertex(0), *gd->searchVertex(3)));
+	gd->add(gd->link(*gd->searchVertex(3), *gd->searchVertex(4)));
+	gd->add(gd->link(*gd->searchVertex(3), *gd->searchVertex(5)));
+	gd->add(gd->link(*gd->searchVertex(2), *gd->searchVertex(6)));
+
 	gd->display();
 	cout << "\n\n\n";
-	gd->display(*e[4]);
+
+	gd->display(*gd->searchEdge(3));
 	cin.get();
 return 0;
 }
